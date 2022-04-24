35d648ee85191179f589846e66817d6452776c2c
Campbell Barton <ideasman42@gmail.com>
2022-04-25 09:59:24 +1000

    Correct description in the header

--- py-autopep8.el
+++ py-autopep8.el
@@ -13,7 +13,7 @@
 
 ;;; Commentary:
 
-;; Provides the `py-autopep8' command, which uses the external "autopep8"
+;; Provides the `py-autopep8-buffer' command, which uses the external "autopep8"
 ;; tool to tidy up the current buffer according to Python's PEP8.
 
 ;;; Usage
