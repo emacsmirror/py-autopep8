40b0288bf780e308e9760d3dba1fa7bbd14113f9
Campbell Barton <ideasman42@gmail.com>
2022-05-03 10:10:28 +1000

    Tests: don't leave temporary file behind
    Also remove unused travis functions

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,17 +2,9 @@
 
 TEST_FILE=/tmp/py-test-file.py
 
-
-install_emacs24() {
-    sudo add-apt-repository ppa:cassou/emacs -y
-    sudo apt-get update -y
-    sudo apt-get install emacs24 -y
-}
-
-
 test_01() {
     echo $FUNCNAME
-    rm $TEST_FILE || true
+    rm -f $TEST_FILE
     emacs --no-init-file -nw \
           --load ./tests/tests.el \
           --load py-autopep8.el \
@@ -22,12 +14,13 @@ test_01() {
           -f kill-emacs
 
     diff -u $TEST_FILE ./tests/01/after.py
+    rm $TEST_FILE || true
 }
 
 
 test_02() {
     echo $FUNCNAME
-    rm $TEST_FILE || true
+    rm -f $TEST_FILE
     emacs --no-init-file -nw \
           --load ./tests/tests.el \
           --load ./tests/02/init.el  \
@@ -38,12 +31,13 @@ test_02() {
           -f kill-emacs
 
     diff -u $TEST_FILE ./tests/02/after.py
+    rm $TEST_FILE || true
 }
 
 
 test_03() {
     echo $FUNCNAME
-    rm $TEST_FILE || true
+    rm -f $TEST_FILE
     emacs --no-init-file -nw \
           --load ./tests/tests.el \
           --load ./tests/03/init.el  \
@@ -54,12 +48,13 @@ test_03() {
           -f kill-emacs
 
     diff -u $TEST_FILE ./tests/03/after.py
+    rm $TEST_FILE || true
 }
 
 
 test_04() {
     echo $FUNCNAME
-    rm $TEST_FILE || true
+    rm -f $TEST_FILE
     emacs --no-init-file -nw \
           --load ./tests/tests.el \
           --load py-autopep8.el \
@@ -69,12 +64,13 @@ test_04() {
           -f kill-emacs
 
     diff -u $TEST_FILE ./tests/04/after.py
+    rm $TEST_FILE || true
 }
 
 
 test_05() {
     echo $FUNCNAME
-    rm $TEST_FILE || true
+    rm -f $TEST_FILE
     emacs --no-init-file -nw \
           --load ./tests/tests.el \
           --load ./tests/05/init.el \
@@ -84,29 +80,15 @@ test_05() {
           -f kill-emacs
 
     diff -u $TEST_FILE ./tests/05/after.py
+    rm $TEST_FILE || true
 }
-
-
-test_install_package() {
-    emacs --no-init-file -nw \
-          py-autopep8.el \
-          -f package-install-from-buffer \
-          -f kill-emacs
-}
-
 
 main() {
-    if [ "$TRAVIS" = "true" ]; then
-        install_emacs24
-        test_install_package
-    fi
-
     test_01
     test_02
     test_03
     test_04
     test_05
 }
-
 
 main
