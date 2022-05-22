cf2daa75aee75d4dec772103c577c614c85409ad
Campbell Barton <ideasman42@gmail.com>
2022-05-22 21:53:49 +1000

    Tests: run in batch mode
    There is no real advantage to running in an emacs window, use "--batch"
    mode.

--- tests/tests.el
+++ tests/tests.el
@@ -1,2 +1,9 @@
-(defun write-test-file ()
-  (write-file "/tmp/py-test-file.py"))
+(defun py-autopep8-tests-setup ()
+  ;; Disable undo (for faster tests).
+  (setq buffer-undo-list t)
+
+  ;; Quiet warning message about guessing offset.
+  (setq python-indent-guess-indent-offset t)
+  (setq python-indent-guess-indent-offset-verbose nil))
+
+(defun py-autopep8-write-test-file () (write-file "/tmp/py-test-file.py"))
--- tests/tests.sh
+++ tests/tests.sh
@@ -2,15 +2,19 @@
 
 TEST_FILE=/tmp/py-test-file.py
 
+# Can also be `-nw`.
+BACKGROUND_FLAG="--batch"
+
 test_01() {
     echo $FUNCNAME
     rm -f $TEST_FILE
-    emacs --no-init-file -nw \
+    emacs --no-init-file $BACKGROUND_FLAG \
           --load ./tests/tests.el \
+          -f py-autopep8-tests-setup \
           --load py-autopep8.el \
           ./tests/01/before.py \
           -f py-autopep8-buffer \
-          -f write-test-file \
+          -f py-autopep8-write-test-file \
           -f kill-emacs
 
     diff -u $TEST_FILE ./tests/01/after.py
@@ -21,13 +25,14 @@ test_01() {
 test_02() {
     echo $FUNCNAME
     rm -f $TEST_FILE
-    emacs --no-init-file -nw \
+    emacs --no-init-file $BACKGROUND_FLAG \
           --load ./tests/tests.el \
+          -f py-autopep8-tests-setup \
           --load ./tests/02/init.el  \
           --load py-autopep8.el \
           ./tests/02/before.py \
           -f py-autopep8-buffer \
-          -f write-test-file \
+          -f py-autopep8-write-test-file \
           -f kill-emacs
 
     diff -u $TEST_FILE ./tests/02/after.py
@@ -38,13 +43,14 @@ test_02() {
 test_03() {
     echo $FUNCNAME
     rm -f $TEST_FILE
-    emacs --no-init-file -nw \
+    emacs --no-init-file $BACKGROUND_FLAG \
           --load ./tests/tests.el \
+          -f py-autopep8-tests-setup \
           --load ./tests/03/init.el  \
           --load py-autopep8.el \
           ./tests/03/before.py \
           -f py-autopep8-buffer \
-          -f write-test-file \
+          -f py-autopep8-write-test-file \
           -f kill-emacs
 
     diff -u $TEST_FILE ./tests/03/after.py
@@ -55,12 +61,13 @@ test_03() {
 test_04() {
     echo $FUNCNAME
     rm -f $TEST_FILE
-    emacs --no-init-file -nw \
+    emacs --no-init-file $BACKGROUND_FLAG \
           --load ./tests/tests.el \
+          -f py-autopep8-tests-setup \
           --load py-autopep8.el \
           ./tests/04/before.py \
           -f py-autopep8-buffer \
-          -f write-test-file \
+          -f py-autopep8-write-test-file \
           -f kill-emacs
 
     diff -u $TEST_FILE ./tests/04/after.py
@@ -71,12 +78,13 @@ test_04() {
 test_05() {
     echo $FUNCNAME
     rm -f $TEST_FILE
-    emacs --no-init-file -nw \
+    emacs --no-init-file $BACKGROUND_FLAG \
           --load ./tests/tests.el \
+          -f py-autopep8-tests-setup \
           --load ./tests/05/init.el \
           --load py-autopep8.el \
           ./tests/05/before.py \
-          -f write-test-file \
+          -f py-autopep8-write-test-file \
           -f kill-emacs
 
     diff -u $TEST_FILE ./tests/05/after.py
